f8ebd8ab6a43598c2c1e354c1abcbfef542d9642
balabeltmimi <thanaporn.jirawat@gmail.com>
2021-08-24 04:15:38 +0700

    :zap: add to-do task input

--- src/App.js
+++ src/App.js
@@ -2,29 +2,70 @@ import React, { useState } from "react";
 import "./App.css";
 
 function Todo({ todo }) {
+
   return (
-    <div className="todo">
-      {todo.text}
-    </div>
+    <>
+      <ul>
+        <li>
+          {todo.title}: 
+          {todo.description}
+        </li>
+      </ul>
+    </>
+  )
+}
+
+function AddTodo ({ addTodo }) {
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+
+  const clickHandler = (e) => {
+    e.preventDefault()
+
+    addTodo({ title, description })
+  }
+
+  const titleChange = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const descriptionChange = (e) => {
+    setDescription(e.target.value)
+  }
+
+  return (
+    <>
+    <input value={ title } onChange={ titleChange } type="text" placeholder="Title" />
+    <input value={ description } onChange={ descriptionChange } type="text" placeholder="Description" />
+    <button onClick={ clickHandler }>Add</button>
+    </>
   )
 }
 
 function App() {
-  const [todos, setTodos] = useState([
-    { text: "one" },
-    { text: "two" }
-  ])
+  const [todos, setTodos] = useState([{
+    title: "test_title" ,
+    description: "test_description"
+  }])
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo])
+  }
+
+  const removeTodo = (index) => {
+    setTodos([...todos.slice(0, index), ...todos.slice(index + 1)])
+  }
 
   return (
-    <div className="app">
-      <div className="todo-list">
+    <>
+    <AddTodo addTodo={ addTodo }/>
         {todos.map((todo, index) => (
-          <Todo
+          <Todo 
             todo={todo}
+            key={index}
           />
         ))}
-      </div>
-    </div>
+    </>
   );
 }
 
